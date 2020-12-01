46815b538d56e36c9b0925fccdcb195e3a2e4f98
Николаев Андрей <Abramson21@mail.ru>
2020-12-01 15:03:47 +0300

    система маршрутизации, добавлена логика

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +16,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -24,16 +26,20 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseStaticFiles(new StaticFileOptions(new SharedOptions {  }))
+
+            //Маршрутизация
             app.UseRouting();
 
-            var greetings = _Configuration["greetings"];
-
+            //Обработка подключения
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync(greetings);
-                });
+                endpoints.MapGet("/greetings", async ctx => await ctx.Response.WriteAsync(_Configuration["greetings"]));
+                endpoints.MapGet("/hel", async ctx => await ctx.Response.WriteAsync("Hello"));
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{actrion=Index}/{id?}");
             });
         }
     }
