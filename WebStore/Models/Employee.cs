e4aef0eae8b7815181cc2025ce787f2644b47898
Николаев Андрей <Abramson21@mail.ru>
2020-12-01 15:59:55 +0300

    Первая модель

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    /// <summary>
+    /// Сотрудник
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
