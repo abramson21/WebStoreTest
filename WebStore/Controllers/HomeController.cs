baa284f7d00191286d0908720f458a574c9f2a0c
Николаев Андрей <Abramson21@mail.ru>
2020-12-01 16:34:38 +0300

    БД

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
+        private static readonly List<Employee> _Employee = new()
+        {
+            new Employee() { Id = 1, FirstName = "Иван", LastName = "Иванов", Patronymic = "Иванович", Age = 26 },
+            new Employee() { Id = 1, FirstName = "Петр", LastName = "Петров", Patronymic = "Петрович", Age = 28 },
+            new Employee() { Id = 1, FirstName = "Сидор", LastName = "Сидоров", Patronymic = "Сидорович", Age = 26 },
+        }
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
         public IActionResult Index()
@@ -23,5 +30,11 @@ namespace WebStore.Controllers
         {
             return Content("Second controller");
         }
+
+        public IActionResult Employes()
+        {
+            return View();
+        }
     }
+
 }
